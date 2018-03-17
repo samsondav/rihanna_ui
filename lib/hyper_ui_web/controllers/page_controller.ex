cb3581307998212610a42b32b5d3732b84b8395f
Sam Davies <sampdavies@gmail.com>
2018-03-17 15:03:53 +0000

    Overview pane

--- lib/hyper_ui_web/controllers/page_controller.ex
+++ lib/hyper_ui_web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule HyperUiWeb.PageController do
   use HyperUiWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    jobs = Hyper.Job
+    |> Hyper.Repo.all()
+    |> Enum.group_by(&(&1.state))
+    enqueued = Enum.count(jobs["ready_to_run"] || [])
+    in_progress = Enum.count(jobs["in_progress"] || [])
+    failed = Enum.count(jobs["failed"] || [])
+
+    render conn, "index.html", enqueued: enqueued, in_progress: in_progress, failed: failed
   end
 end
