844d4334319817e5e70e0398f5dab6ce3d3d8aa1
Max Katz <maxwelljkatz@gmail.com>
2020-11-09 20:42:07 -0700

    Update logging to be less verbose in prod

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :rihanna_ui, RihannaUIWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :warning
 
 # Configure your database
 config :rihanna_ui, RihannaUI.Repo,
